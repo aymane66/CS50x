419ed1e9414294d104e15231bb30ed9d67d16a6e
Ayman MOUHCINE <120111126+aymane66@users.noreply.github.com>
2023-06-27 21:55:17 +0100

    Add files via upload

---
+++ readability/readability.c
@@ -0,0 +1,50 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    printf("--- Reading Level Assesser --- \n");
+    string text = get_string("Text: ");
+
+
+    int i, letters = 0, words = 1, sentences = 0;
+    for (i = 0; i < strlen(text); i ++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+
+
+    }
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
